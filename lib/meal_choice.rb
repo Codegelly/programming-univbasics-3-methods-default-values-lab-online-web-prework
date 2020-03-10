bfb49b9ed76d92a53ac54eb061542110c7d4349d
Angelica  Thompson <i.thompson89@outlook.com>
2020-03-10 16:49:05 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,5 @@
 #meal_choice
+<<<<<<< HEAD
 #def meal_choice(veg1, veg2, protein="meat")
  # return"A plate of #{protein} with #{veg1} and #{veg2}."
 #end
@@ -15,3 +16,13 @@ end
 #def meal_choice(veg1,veg2,whatever)
  # return"A plate of #{whatever} with #{veg1} and #{veg2}."
 #end
+=======
+def meal_choice(veg1, veg2, protein="meat")
+  return"A plate of meat with #{veg1} and #{veg2}."
+end
+
+#meal_choice
+def meal_choice(veg1,veg2,whatever)
+  return"A plate of #{whatever} with #{veg1} and #{veg2}."
+end
+>>>>>>> e9520b5ac87f28e5ce26c9047b6efeecb9992d23
